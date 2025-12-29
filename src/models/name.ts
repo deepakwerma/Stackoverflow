dad8279533ee2264313187e5dfc5df230d2354a9
deepakwerma <deepakrvermaa@gmail.com>
2025-12-29 17:40:21 +0530

    Delete src directory

--- src/models/name.ts
+++
@@ -1,6 +0,0 @@
-export const db = "main-stackflow";
-export const questionCollection = "questions";
-export const answerCollection = "answers";
-export const commentCollection = "comments";
-export const voteCollection = "votes";
-export const questionAttachmentBucket = "question-attachment";
